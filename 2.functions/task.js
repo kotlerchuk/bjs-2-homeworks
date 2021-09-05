12330e8d86edff6e6621ff05ce02d099fcdc77a2
Mary Kotler <m.kotlerchuk@gmail.com>
2021-09-05 17:55:54 +0300

    first commit variables

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,75 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg = 0;
 
-  // Ваш код
+ for (let i = 0; i < arr.length; i++) {
+   if (max < arr[i]) {
+     max = arr[i];
+   };
+   if (min > arr[i]) {
+    min = arr[i];
+  };
+  sum += arr[i];
+ }
 
-  return { min:min, max:max, avg:avg };
-}
+ avg = sum / arr.length;
+ avg = Number(avg.toFixed(2));
+
+  return { min, max, avg };
+};
 
 // Задание 2
 function worker(arr) {
-  let sum;
+	let sum = 0;
 
-  // Ваш код
+	// Ваш код
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	};
 
-  return sum;
-}
+	return sum;
+};
 
 function makeWork(arrOfArr, func) {
-  let max
+	let max = -Infinity;
+	let min = Infinity;
 
-  // Ваш кода
-  // for ...
-  
-  return max
-}
+	// Ваш код
+
+	for (let j = 0; j < arrOfArr.length; j++) {
+
+		if (max < func(arrOfArr[j])) {
+			max = func(arrOfArr[j]);
+		};
+		if (min > func(arrOfArr[j])) {
+			min = func(arrOfArr[j]);
+		};
+
+	};
+
+	return max;
+};
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
-}
+
+	// Ваш код
+	let max = -Infinity;
+	let min = Infinity;
+
+	for (let i = 0; i < arr.length; i++) {
+
+		if (max < arr[i]) {
+			max = arr[i];
+		};
+		if (min > arr[i]) {
+			min = arr[i];
+		};
+	};
+
+	return max - min;
+
+};
