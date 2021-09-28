782433f8051ce6dc264f5a329c5679d57c2ab5ae
Mary Kotler <m.kotlerchuk@gmail.com>
2021-09-28 12:29:45 +0300

    first commit variables

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,11 @@
 function compareArrays(arr1, arr2) {
-  let result;
 
-  // Ваш код
+	// Ваш код
 
-  return result; // boolean
-}
+	return arr1.length === arr2.length && arr1.every((item, idx) => arr1[idx] === arr2[idx]);
+};
+
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+	return arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
 }
