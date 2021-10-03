911835bea2af08e527828a04e84f43115342fc63
Mary Kotler <m.kotlerchuk@gmail.com>
2021-10-03 20:40:13 +0300

    first commit variables

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,56 @@
 function Student(name, gender, age) {
-    // Ваш код
-
-}
+	// Ваш код
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+};
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+	//ваш код
+	this.subject = subjectName;
+};
 
 // ваш код для остальных методов
+
+
+Student.prototype.addMark = function (mark) {
+	//ваш код
+
+	if (this.marks === undefined) {
+		// добавить первую оценку 
+		this.marks = [mark];
+
+	} else {
+		// добавить вторую и последующие оценки в массив
+		this.marks.push(mark);
+	};
+};
+
+Student.prototype.addMarks = function (...mark) {
+	//ваш код
+
+	if (this.marks === undefined) {
+		// добавить первую оценку 
+		this.marks = [];
+		this.marks.push(...mark);
+
+	} else {
+		// добавить вторую и последующие оценки в массив
+		this.marks.push(...mark);
+	};
+};
+
+
+Student.prototype.getAverage = function () {
+	return this.marks.reduce((acc, item) => acc += item) / this.marks.length;
+};
+
+
+Student.prototype.exclude = function (reason) {
+	//ваш код
+
+	delete this.subject;
+	delete this.marks;
+
+	this.excluded = reason;
+};
